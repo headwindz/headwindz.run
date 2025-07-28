3aab6c3d3717b9f146eab741d3e83f4190324896
headwindz <zhengjianhua.michael@gmail.com>
2025-07-28 22:54:54 +0800

    chore: update index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,11 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5in ⏱️
+            to="/blog">
+          Building, Open soon :-)
           </Link>
         </div>
       </div>
@@ -31,7 +30,13 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 60 }}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/blog">
+            Building, Open soon :-)
+          </Link>
+        </div>    
     </Layout>
   );
 }
